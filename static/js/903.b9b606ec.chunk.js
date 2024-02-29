c91a02fe38443c582b52af592a49eb5d18ed4ef9
SergeyMikhailuk <63360366+SergeyMikhailuk@users.noreply.github.com>
2024-02-29 20:34:51 +0000

    Deploying to gh-pages from @ SergeyMikhailuk/TaskPro-FrontEnd-Team5@3338534b520ef2dd519734d02d3a216b30906de2 🚀

---
+++ static/js/903.b9b606ec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[903],{903:function(e,t,r){r.r(t);r(791);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"Home"})})}}}]);
+//# sourceMappingURL=903.b9b606ec.chunk.js.map
