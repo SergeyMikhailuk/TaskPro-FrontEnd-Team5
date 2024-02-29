fbe27cdc008918b4c99c511412af8e92940858ac
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-02-29 21:24:48 +0000

    fix basename 4

--- src/containers/routes/index.jsx
+++ src/containers/routes/index.jsx
@@ -4,14 +4,10 @@ import { Route, Navigate } from 'react-router-dom';
 import Layout from 'components/Layout';
 
 const Home = lazy(() => import('containers/pages/Home'));
-const TestDeployedRouting = lazy(
-  () => import('containers/pages/TestDeployedRouting')
-);
 
 const routing = () => (
   <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
-    <Route path="test" element={<TestDeployedRouting />} />
     <Route path="*" element={<Navigate to="/" />} />
   </Route>
 );
