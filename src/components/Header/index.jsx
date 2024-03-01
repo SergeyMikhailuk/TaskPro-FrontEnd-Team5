addb5aff0f14e5068c7a1f424f010c28e1ce1068
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-03-01 10:14:56 +0000

    example svg import

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { ReactComponent as PresentIcon } from 'images/present-icon.svg';
+
 import { AppHeader } from './styled';
 
 const Header = () => {
   return (
     <AppHeader>
       <h2>Header</h2>
+      <PresentIcon />
     </AppHeader>
   );
 };
