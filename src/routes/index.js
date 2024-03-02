d1e3375d95a2fa06964ad50adcd70e1f96c87c00
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-03-02 11:42:43 +0000

    remove Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,10 @@ import { Route, Navigate } from 'react-router-dom';
 
 import Layout from 'components/Layout';
 
-const Home = lazy(() => import('pages/Home'));
 const Welcome = lazy(() => import('pages/Welcome'));
 
 const routing = () => (
   <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
     <Route path="welcome" element={<Welcome />} />
     <Route path="*" element={<Navigate to="/" />} />
   </Route>
