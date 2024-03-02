28edd17c0f5d2e52c0611448850ef356001dd380
mariashubna <mariashubna@gmail.com>
2024-03-02 12:57:03 +0100

    Header-create

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+// import Select from 'react-select';
 
-import { ReactComponent as PresentIcon } from 'images/present-icon.svg';
+import user from '../../images/user.png';
 
-import { AppHeader } from './styled';
+import { AppHeader, Wrap, Select, Burger, Info } from './styled';
+
+const options = [
+  { value: 'light', label: 'Light' },
+  { value: 'dark', label: 'Dark' },
+  { value: 'violet', label: 'Violet' },
+];
 
 const Header = () => {
   return (
     <AppHeader>
-      <h2>Header</h2>
-      <PresentIcon />
+      <Burger />
+      <Info>
+        <Select options={options} placeholder="Theme" />
+        <Wrap>
+          <p>Name</p>
+          <img src={user} alt="" />
+        </Wrap>
+      </Info>
     </AppHeader>
   );
 };
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,15 +1,50 @@
 import styled from 'styled-components';
-import { colors } from 'constants/colors';
+import ReactSelect from 'react-select';
 
+import { ReactComponent as BurgerImage } from './menu.svg';
+
+export const Burger = styled(BurgerImage)`
+  width: 32px;
+  height: 32px;
+  stroke: white;
+`;
+export const Select = styled(ReactSelect)`
+/* width: 68px; */
+
+`;
 export const AppHeader = styled.header`
   display: flex;
   align-items: center;
-  width: 100%;
-  padding: 12px;
-  background-color: black;
 
-  h2 {
-    color: ${colors.green};
+  width: 100%;
+  padding: 18px 24px;
+  background-color: #161616;
+  justify-content: space-between;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  p {
     font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    letter-spacing: -0.02em;
+    color: #fff;
+  }
+`;
+
+export const Image = styled.div`
+  svg {
+    width: 32px;
+    height: 32px;
+    stroke: white;
   }
 `;
