bfb22275f2d24a2ea67caa04feaf2f63d2bcc049
VolodymyrBab <volbv@yahoo.com>
2024-03-03 13:27:08 +0100

    welcomePage+App

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { store } from 'redux/store';
+import { App } from 'App';
 import 'modern-normalize';
 import 'styles/base.css';
 
-import routing from 'routes';
+const root = document.getElementById('root');
 
-const router = createBrowserRouter(createRoutesFromElements(routing()), {
-  basename: '/TaskPro-FrontEnd-Team5',
-});
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(root).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <BrowserRouter basename="/TaskPro-FrontEnd-Team6">
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
