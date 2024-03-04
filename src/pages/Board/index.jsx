ca4c392b6f67874888ca10abc5c84242fb5748be
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2024-03-04 14:26:00 -0700

    card markup

--- src/pages/Board/index.jsx
+++ src/pages/Board/index.jsx
@@ -1,13 +1,57 @@
-import { Title, TestIcon } from './styled';
+import { Title, TestIcon, HeaderList, TaskCard } from './styled';
 import testImg from 'images/2.png';
+
 const BoardPage = () => {
   return (
     <>
       <Title>Board Page</Title>
-
-      <img src={testImg} alt="" />
-      <TestIcon />
-      <p>test</p>
+      <HeaderList>
+        <li>
+          <div class="head-container">
+            <h2>To Do</h2>
+          </div>
+        </li>
+        <div class="icon-container">
+          <li>
+            <a href="">
+              <svg width="16" height="16">
+                <use xlinkHref="images/sprite.svg#icon-pencil"></use>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <svg width="16" height="16">
+                <use xlinkHref="images/sprite.svg#icon-trash"></use>
+              </svg>
+            </a>
+          </li>
+        </div>
+      </HeaderList>
+      <TaskCard>
+        <li>
+          <div>
+            <h2>The Watch Spot Design</h2>
+            <p>
+              Create a visually stunning and eye-catching watch dial design that
+              embodies our brand's essence of sleek aesthetics and modern
+              elegance. Your design should be unique, innovative, and reflective
+              of the latest trends in watch design.
+            </p>
+            <h3></h3>
+            <div></div>
+            <h3></h3>
+            <p></p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </li>
+      </TaskCard>
+      {/* <img src={testImg} alt="" /> */}
+      {/* <TestIcon /> */}
     </>
   );
 };
--- src/pages/Board/styled.js
+++ src/pages/Board/styled.js
@@ -11,3 +11,46 @@ export const TestIcon = styled(TestSVG)`
   width: 30px;
   height: 20px;
 `;
+
+export const HeaderList = styled.ul`
+  width: 334px;
+  height: 56px;
+  background-color: #121212;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 20px;
+  margin-left: 20px;
+  border-radius: 8px;
+  font-family: 'Poppins', sans-serif;
+  letter-spacing: -0.02em;
+  line-height: 21px;
+
+  li {
+  }
+  h2 {
+    font-size: 14px;
+    color: #ffffff;
+  }
+  a {
+    background-color: #ffffff;
+  }
+  .head-container {
+    display: flex;
+  }
+  .icon-container {
+    display: flex;
+    gap: 8px;
+
+    padding-right: 12px;
+  }
+`;
+
+export const TaskCard = styled.ul`
+  width: 334px;
+  background-color: #121212;
+  h2 {
+    font-size: 14px;
+    color: #ffffff;
+  }
+`;
