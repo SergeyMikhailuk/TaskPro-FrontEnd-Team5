bfd795c222af959edb94042b69a4617acf2df029
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2024-03-04 14:59:14 +0200

    editCard

---
+++ src/components/Card/AddCardModal/CardModal.styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { Form, Field } from 'formik';
+
+export const ModalSection = styled.div``;
+
+export const Title = styled.h2``;
+
+export const FormWrapper = styled.div``;
+
+export const LabelInput = styled.label`
+  width: 100%;
+`;
+
+export const TitleInput = styled(Field)``;
+
+export const AuthFormSubmitButton = styled.button``;
+
+export const DefaultRadioBtn = styled(Field)``;
+
+export const FormTitle = styled.h3``;
+
+export const RadioBtnWrapper = styled.ul``;
+
+export const Label = styled.label``;
+
+export const LabelItem = styled.div``;
+
+export const ButtonPlus = styled.div``;
+
+export const PlusIcon = styled.svg``;
+
+export const ModalForm = styled(Form)``;
+
+export const Wrapper = styled.div``;
+
+export const DateTitle = styled.div``;
