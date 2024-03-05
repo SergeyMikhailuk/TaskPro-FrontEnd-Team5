540b1be11705a86da4b316e385846e60bf177326
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2024-03-05 14:05:19 -0700

    1

--- src/pages/Board/index.jsx
+++ src/pages/Board/index.jsx
@@ -1,54 +1,36 @@
-import { Title, HeaderList, TaskCard } from './styled';
+import {
+  Title,
+  ColumnName,
+  Wrapper,
+  TitleWrapper,
+  TitleHeader,
+  IconWrapper,
+  Icon,
+} from './styled';
 // import testImg from 'images/2.png';
 
 const BoardPage = () => {
   return (
     <>
       <Title>Board Page</Title>
-      <HeaderList>
-        <li>
-          <div class="head-container">
-            <h2>To Do</h2>
-          </div>
-        </li>
-        <div class="icon-container">
-          <li class="svg-icon">
-            <a href="">
-              <svg width="16" height="16">
-                <use xlinkHref="images/sprite.svg#icon-pencil"></use>
-              </svg>
-            </a>
+      <Wrapper>
+        <ColumnName>
+          <li>
+            <TitleWrapper>
+              <TitleHeader>To Do</TitleHeader>
+            </TitleWrapper>
           </li>
-          <li class="svg-icon">
-            <a href="">
-              <svg width="16" height="16">
-                <use xlinkHref="images/sprite.svg#icon-trash"></use>
-              </svg>
-            </a>
+          <li>
+            <IconWrapper>
+              <a href="/">
+                <Icon>
+                  <use xlinkHref="images/sprite.svg#icon-pencil"></use>
+                </Icon>
+              </a>
+            </IconWrapper>
           </li>
-        </div>
-      </HeaderList>
-      <TaskCard>
-        <li>
-          <div>
-            <h2>The Watch Spot Design</h2>
-            <p class="task-description">
-              Create a visually stunning and eye-catching watch dial design that
-              embodies our brand's
-            </p>
-            <div class="line"></div>
-            <h3>Priority</h3>
-            <div class="priority-color"></div>
-            <h3>Deadline</h3>
-            <p class="deadline">12/05/2022</p>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-        </li>
-      </TaskCard>
+        </ColumnName>
+      </Wrapper>
     </>
   );
 };
@@ -60,3 +42,52 @@ export default BoardPage;
 {
   /* <TestIcon /> */
 }
+// <HeaderList>
+//       <li>
+//         <div class="head-container">
+//           <h2>To Do</h2>
+//         </div>
+//       </li>
+//       <div class="icon-container">
+//         <li class="svg-icon">
+//           <a href="">
+//             <svg>
+//               <use xlinkHref="images/sprite.svg#icon-pencil"></use>
+//             </svg>
+//           </a>
+//         </li>
+//         <li class="svg-icon">
+//           <a href="">
+//             <svg>
+//               <use xlinkHref="images/sprite.svg#icon-trash"></use>
+//             </svg>
+//           </a>
+//         </li>
+//       </div>
+//     </HeaderList>
+
+//     <TaskCard>
+//       <li>
+//         <div>
+//           <h2>The Watch Spot Design</h2>
+//           <p class="task-description">
+//             Create a visually stunning and eye-catching watch dial design
+//             that embodies our brand's
+//           </p>
+//           <div class="priority-wrapper">
+//             <h3>Priority</h3>
+//             <div class="priority-color"></div>
+//           </div>
+//           <div class="deadline-wrapper">
+//             <h3>Deadline</h3>
+//             <p class="deadline">12/05/2022</p>
+//           </div>
+
+//           <ul>
+//             <li></li>
+//             <li></li>
+//             <li></li>
+//           </ul>
+//         </div>
+//       </li>
+//     </TaskCard>
--- src/pages/Board/styled.js
+++ src/pages/Board/styled.js
@@ -7,12 +7,12 @@ export const Title = styled.h1`
   color: ${colors.green};
   font-size: 50px;
 `;
+export const Wrapper = styled.div``;
 // export const TestIcon = styled(TestSVG)`
 //   width: 30px;
 //   height: 20px;
 // `;
-
-export const HeaderList = styled.ul`
+export const ColumnName = styled.ul`
   width: 334px;
   height: 56px;
   background-color: #121212;
@@ -25,91 +25,140 @@ export const HeaderList = styled.ul`
   font-family: 'Poppins', sans-serif;
   letter-spacing: -0.02em;
   line-height: 21px;
-
   color: #ffffff;
+`;
+export const TitleWrapper = styled.div``;
 
-  li {
-  }
-  h2 {
-    font-size: 14px;
-  }
-  a {
-    display: block;
-    width: 16px;
-    height: 16px;
-  }
-  .svg-icon {
-    background-color: #ffffff;
-    opacity: 50%;
-  }
-  .head-container {
-    display: flex;
-  }
-  .icon-container {
-    display: flex;
-    gap: 8px;
+export const TitleHeader = styled.h2`
+  font-size: 14px;
+`;
+export const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
+export const Icon = styled.svg`
+  width: 16px;
+  height: 16px;
+  /* stroke: ${props => props.theme.column.icon}; */
+  fill: transparent;
+  cursor: pointer;
+  transition: all 250ms linear;
 
-    padding-right: 12px;
+  &:hover {
+    opacity: 0.4;
+    transform: scale(1.4);
   }
 `;
+// export const HeaderList = styled.ul`
+//   width: 334px;
+//   height: 56px;
+//   background-color: #121212;
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   padding-left: 20px;
+//   margin-left: 20px;
+//   border-radius: 8px;
+//   font-family: 'Poppins', sans-serif;
+//   letter-spacing: -0.02em;
+//   line-height: 21px;
 
-export const TaskCard = styled.ul`
-  width: 334px;
-  background-color: #121212;
-  margin-left: 20px;
-  margin-top: 20px;
-  h2 {
-    font-size: 14px;
-    color: #ffffff;
-  }
+//   color: #ffffff;
 
-  h2 {
-    padding-left: 24px;
-    padding-top: 14px;
-    font-weight: 600;
-    line-height: 21px;
-    letter-spacing: -0.02em;
-  }
-  .task-description {
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 16px;
-    letter-spacing: -0.02em;
-    width: 290px;
-    color: #ffffff;
-    opacity: 50%;
-    padding-left: 24px;
-    padding-top: 17px;
-  }
-  .task-description::after {
-    content: '';
-    position: absolute;
-    width: 290px;
-    top: 68%;
-    left: 305px;
-    border: 1px solid #ffffff;
-    opacity: 10%;
-  }
-  h3 {
-    color: #ffffff;
-    opacity: 50%;
-  }
-  .priority-color {
-    width: 12px;
-    height: 12px;
-    background-color: #8fa1d0;
-    border-radius: 50%;
-    font-weight: 400;
-    line-height: 16px;
-    letter-spacing: -0.02em;
-    font-size: 12px;
-  }
-  /* .line {
-    width: 290px;
-    border: 1px #ffffff;
-    opacity: 10%;
-  } */
-  .deadline {
-    color: #ffffff;
-  }
-`;
+//   li {
+//   }
+//   h2 {
+//     font-size: 14px;
+//   }
+//   a {
+//   }
+//   .svg-icon {
+//     width: 16px;
+//     height: 16px;
+//     stroke: darkblue;
+//     fill: #ffffff;
+//     fill: transparent;
+//     cursor: pointer;
+//     transition: all 250ms linear;
+
+//     &:hover {
+//       opacity: 0.4;
+//       transform: scale(1.4);
+//     }
+//   }
+//   .head-container {
+//     display: flex;
+//   }
+//   .icon-container {
+//     display: flex;
+//     gap: 8px;
+
+//     padding-right: 12px;
+//   }
+// `;
+
+// export const TaskCard = styled.ul`
+//   width: 334px;
+//   background-color: #121212;
+//   margin-left: 20px;
+//   margin-top: 20px;
+//   h2 {
+//     font-size: 14px;
+//     color: #ffffff;
+//   }
+
+//   h2 {
+//     padding-left: 24px;
+//     padding-top: 14px;
+//     font-weight: 600;
+//     line-height: 21px;
+//     letter-spacing: -0.02em;
+//   }
+//   .task-description {
+//     font-weight: 400;
+//     font-size: 12px;
+//     line-height: 16px;
+//     letter-spacing: -0.02em;
+//     width: 290px;
+//     color: #ffffff;
+//     opacity: 50%;
+//     padding-left: 24px;
+//     padding-top: 17px;
+//   }
+//   .task-description::after {
+//     content: '';
+//     position: absolute;
+//     width: 100%;
+//     height: 1px;
+//     top: 68%;
+//     left: 305px;
+//     /* border: 1px solid #ffffff; */
+//     /* opacity: 10%; */
+//     /* background-color: rgba(255, 255, 255, 0.1); */
+//     background-color: red;
+//   }
+//   h3 {
+//     color: #ffffff;
+//     opacity: 50%;
+//   }
+//   .priority-color {
+//     width: 12px;
+//     height: 12px;
+//     background-color: #8fa1d0;
+//     border-radius: 50%;
+//     font-weight: 400;
+//     line-height: 16px;
+//     letter-spacing: -0.02em;
+//     font-size: 12px;
+//   }
+//   /* .line {
+//     width: 290px;
+//     border: 1px #ffffff;
+//     opacity: 10%;
+//   } */
+//   .deadline {
+//     color: #ffffff;
+//   }
+// `;
