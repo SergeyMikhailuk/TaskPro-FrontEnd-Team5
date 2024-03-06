a7f8107790e8a68649031cf130ad87e00898912b
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-03-06 18:03:42 +0000

    add home

--- src/components/CabinetLayout/styled.js
+++ src/components/CabinetLayout/styled.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const LayoutWrapper = styled.div`
   display: flex;
   width: 100%;
-  background-color: #1f1f1f;
 `;
 
 export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  background-color: ${props => props.theme.container.background};
 `;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import { Title } from './styled';
+import { Description, HomeWrapper, Accent } from './styled';
 
 const HomePage = () => {
   return (
-    <>
-      <Title>Home Page</Title>
-      <Link to={'/welcome'}>welcome link</Link>
-      <p>test</p>
-    </>
+    <HomeWrapper>
+      <Description>
+        Before starting your project, it is essential{' '}
+        <Accent>to create a board</Accent> to visualize and track all the
+        necessary tasks and milestones. This board serves as a powerful tool to
+        organize the workflow and ensure effective collaboration among team
+        members.
+      </Description>
+    </HomeWrapper>
   );
 };
 
