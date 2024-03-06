8bef02477f7d31284993fc66b0f3196afd4145de
rooddeeo <rooddeeo@gmail.com>
2024-03-07 00:37:19 +0200

    Update sidebar

--- src/components/Sidebar/Cards/index.js
+++ src/components/Sidebar/Cards/index.js
@@ -1,26 +1,37 @@
 import {
   CardsBoardList,
+  CardsBoardListBox,
   CardsBoardListTitle,
   CardsBoardListIcon,
   CardsBoardListBtnGroup,
   CardsBoardListEditBtn,
+  CardsBoardListBtnEdit,
   CardsBoardListDeleteBtn,
+  CardsBoardListBtnDelete,
+  CardsBoardListLine,
 } from './styled';
 
 const Cards = ({ cards, deleteCard }) => {
   console.log(cards.icon);
   return (
     <CardsBoardList>
-      <CardsBoardListTitle>
-        <CardsBoardListIcon>{cards.icon}</CardsBoardListIcon>
-        {cards.title}
-      </CardsBoardListTitle>
-      <CardsBoardListBtnGroup>
-        <CardsBoardListEditBtn></CardsBoardListEditBtn>
-        <CardsBoardListDeleteBtn
-          onClick={() => deleteCard(cards.id)}
-        ></CardsBoardListDeleteBtn>
-      </CardsBoardListBtnGroup>
+      <CardsBoardListBox>
+        <CardsBoardListTitle>
+          <CardsBoardListIcon>{cards.icon}</CardsBoardListIcon>
+          {cards.title}
+        </CardsBoardListTitle>
+        <CardsBoardListBtnGroup>
+          <CardsBoardListEditBtn>
+            <CardsBoardListBtnEdit></CardsBoardListBtnEdit>
+          </CardsBoardListEditBtn>
+          <CardsBoardListDeleteBtn>
+            <CardsBoardListBtnDelete
+              onClick={() => deleteCard(cards.id)}
+            ></CardsBoardListBtnDelete>
+          </CardsBoardListDeleteBtn>
+        </CardsBoardListBtnGroup>
+      </CardsBoardListBox>
+      <CardsBoardListLine></CardsBoardListLine>
     </CardsBoardList>
   );
 };
