8724d7c2ddcb935422ae15fc2c3bfb71e8f2e11d
Mikhaylyuk Sergey <s.mikhaylyuk@hacken.io>
2024-03-11 22:07:33 +0000

    active board redux

---
+++ src/store/activeBoardSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const activeBoardIdSlice = createSlice({
+  name: 'activeBoardId',
+  initialState: '',
+  reducers: {
+    setTheme: (_, action) => action.payload,
+  },
+});
+
+export const { setActiveBoardId } = activeBoardIdSlice.actions;
+
+export const getActiveBoardId = state => state.activeBoardId;
